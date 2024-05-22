ca1897923689ae8f9438d00fa0d395b032564f9b
seoAreum <1971236@hansung.ac.kr>
2024-05-23 04:45:23 +0900

    chore: 주석 추가

--- krampoline/src/libs/hooks/useKakaoLogin.jsx
+++ krampoline/src/libs/hooks/useKakaoLogin.jsx
@@ -7,6 +7,8 @@ const useKakaoLogin = () => {
     if (CODE) {
       api.post(GET_ACCESS_TOKEN_URL).then((res) => {
         const { access_token } = res.data;
+
+        // 서버에 보낼 데이터들 (추후 서버 통신 코드로 대체 예정)
         console.log(access_token, CODE);
       });
     }
