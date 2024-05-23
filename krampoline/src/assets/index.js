5c3100b2f8200f885d5fded57b67df356f272547
seoAreum <1971236@hansung.ac.kr>
2024-05-23 19:56:43 +0900

    style: 아이콘 추가

--- krampoline/src/assets/index.js
+++ krampoline/src/assets/index.js
@@ -1,4 +1,5 @@
 import IcArrowBack from './icon/ic_arrow_back.svg?react';
+import IcCloseGray from './icon/ic_close_gray.svg?react';
 import IcCorrectBlue from './icon/ic_correct_blue.svg?react';
 import IcCorrectGray from './icon/ic_correct_gray.svg?react';
 import IcFailBlue from './icon/ic_fail_blue.svg?react';
@@ -8,6 +9,7 @@ import IcTest from './icon/icon_test.svg?react';
 // svg 더 추가하게 되면 ,(콤마)로 이어서 쓰기
 export {
   IcArrowBack,
+  IcCloseGray,
   IcCorrectBlue,
   IcCorrectGray,
   IcFailBlue,
