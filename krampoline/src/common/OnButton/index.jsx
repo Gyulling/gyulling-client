556a0e7b1b91cf5043f80a1678c2890c97a35ca4
seoAreum <1971236@hansung.ac.kr>
2024-05-23 15:45:59 +0900

    fix: 공통 컴포넌트 props 변경

--- krampoline/src/common/OnButton/index.jsx
+++ krampoline/src/common/OnButton/index.jsx
@@ -1,24 +1,20 @@
 import styled from 'styled-components';
 
-const OnButton = ({children,isActive,disabled}) => {
-    return(
-        <Button isActive={isActive} disabled={disabled}> 
-             {children}
-        </Button>
-    )
+const OnButton = ({ children, disabled }) => {
+  return <Button disabled={disabled}>{children}</Button>;
 };
 
 const Button = styled.button`
-    background-color: ${({ theme,isActive,disabled }) => disabled ? theme.colors.gray300 : isActive ? theme.colors.prime : theme.colors.gray300};
-    color : ${({ theme }) => theme.colors.white};
-    border-radius: 1.6rem;
-    padding: 1.5rem 15.7rem;
-    border: none;
-    ${({ theme }) => theme.fonts.h6}
-    cursor: pointer;
-    &:disabled {
-        cursor: not-allowed;
-    }
-    
-`
+  background-color: ${({ theme, disabled }) =>
+    disabled ? theme.colors.gray300 : theme.colors.prime};
+  color: ${({ theme }) => theme.colors.white};
+  border-radius: 1.6rem;
+  padding: 1.5rem 15.7rem;
+  border: none;
+  ${({ theme }) => theme.fonts.h6}
+  cursor: pointer;
+  &:disabled {
+    cursor: not-allowed;
+  }
+`;
 export default OnButton;
