738552b225fae595b37cd38a022e60e463be998d
seoAreum <1971236@hansung.ac.kr>
2024-05-23 16:25:34 +0900

    chore: 공통 컴포넌트로 변경

--- krampoline/src/pages/SelectPage.jsx
+++ krampoline/src/pages/SelectPage.jsx
@@ -1,12 +1,15 @@
 import styled from 'styled-components';
 import OnButton from '../common/OnButton';
-import Question from '../component/Select/Question';
+import Question from '../common/Question';
 import SelectTag from '../component/Select/SelectTag';
 
 const SelectPage = () => {
   return (
     <SelectPageWrapper>
-      <Question />
+      <Question
+        mainQuestion={'무엇을 좋아하세요?'}
+        subQuestion={'좋아하는 것과 관련한 퀴즈를 풀 수 있습니다.'}
+      />
       <SelectTag />
       {/* disabled 값은 추후에 변경 예정 */}
       <OnButton disabled={false}>다음</OnButton>
