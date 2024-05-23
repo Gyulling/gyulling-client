926949c41309370a8fd0889a5b72c73cf702d59e
Seung taek Oh <elqkddl@naver.com>
2024-05-23 19:42:55 +0900

    style: SVG 아이콘 추가

--- krampoline/src/assets/index.js
+++ krampoline/src/assets/index.js
@@ -1,4 +1,6 @@
+import Bomb from './icon/bomb.svg?react';
 import IcTest from './icon/icon_test.svg?react';
+import SuccessIcon from './icon/success.svg?react';
 
 // svg 더 추가하게 되면 ,(콤마)로 이어서 쓰기
-export { IcTest };
+export { Bomb, IcTest, SuccessIcon };
