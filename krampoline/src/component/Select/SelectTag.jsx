564dbce4d04be9f9d6f3e0498ef72abe1cf77db1
seoAreum <1971236@hansung.ac.kr>
2024-05-23 14:42:04 +0900

    feat: 스크롤 구현

--- krampoline/src/component/Select/SelectTag.jsx
+++ krampoline/src/component/Select/SelectTag.jsx
@@ -1,8 +1,77 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const DUMMY = [
+  '바다',
+  '바다',
+  '바다',
+  '바다',
+  '바다',
+  '바다',
+  '바다',
+  '바다',
+  '바다',
+  '바다',
+  '바다',
+  '바다',
+  '바다',
+  '바다',
+  '바다',
+  '바다',
+  '바다',
+  '바다',
+  '바다',
+  '바다',
+  '바다',
+  '바다',
+  '바다',
+  '바다',
+  '바다',
+  '바다',
+  '바다',
+  '바다',
+  '바다',
+  '바다',
+  '바다',
+  '바다',
+  '바다',
+  '바다',
+  '바다',
+  '바다',
+  '바다',
+  '바다',
+  '바다',
+  '바다',
+  '바다',
+  '바다',
+  '바다',
+  '바다',
+  '바다',
+  '바다',
+  '바다',
+  '바다',
+  '바다',
+  '바다',
+  '바다',
+  '바다',
+  '바다',
+  '바다',
+  '바다',
+  '바다',
+  '바다',
+  '바다',
+  '바다',
+  '바다',
+];
+
 const SelectTag = () => {
-  return <SelectTagWrapper>으아아아아ㅏ</SelectTagWrapper>;
+  return (
+    <SelectTagWrapper>
+      {DUMMY.map((data) => {
+        return <div>{data}</div>;
+      })}
+    </SelectTagWrapper>
+  );
 };
 
 export default SelectTag;
@@ -10,5 +79,9 @@ export default SelectTag;
 const SelectTagWrapper = styled.article`
   width: 100%;
   height: calc(100dvh - 24.4rem);
+  overflow-y: auto;
+
   background-color: ${({ theme }) => theme.colors.prime};
+
+  gap: 5rem;
 `;
