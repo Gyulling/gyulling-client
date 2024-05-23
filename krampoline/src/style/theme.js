8db6d89262eba97ffd1c5c901aa162cdd1104f3d
Seung taek Oh <elqkddl@naver.com>
2024-05-23 11:06:52 +0900

    Style: Color 코드 추가

--- krampoline/src/style/theme.js
+++ krampoline/src/style/theme.js
@@ -1,7 +1,19 @@
 const colors = {
   // 아래 형식으로 정의
   //   white: '#FFFFFF',
-};
+  gray050 : '#F8F9FA',
+  gray100 : '#F3F5F7',
+  gray200 : '#ECEEF0',
+  gray300 : '#E2E5E8',
+  gray400 : '#D4D8DC',
+  gray500 : '#B5B9BD',
+  gray600 : '#8E9398',
+  gray700 : '#505458',
+  gray800 : '#3A3D40',
+  gray900 : '#252729',
+  prime : '#3CC2FF',
+  sub : '#FFB800',
+}
 
 const fonts = {
   // 아래 형식으로 정의
