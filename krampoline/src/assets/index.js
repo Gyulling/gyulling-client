90a83bf1d0ee776602b93d86c48161e46c289fe1
seoAreum <1971236@hansung.ac.kr>
2024-05-23 16:38:19 +0900

    style: icon 추가

--- krampoline/src/assets/index.js
+++ krampoline/src/assets/index.js
@@ -1,4 +1,5 @@
+import IcArrowBack from './icon/ic_arrow_back.svg?react';
 import IcTest from './icon/icon_test.svg?react';
 
 // svg 더 추가하게 되면 ,(콤마)로 이어서 쓰기
-export { IcTest };
+export { IcArrowBack, IcTest };
