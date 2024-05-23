34107620314c41dc4ecff07c1b2c79181c556d28
seoAreum <1971236@hansung.ac.kr>
2024-05-23 19:59:53 +0900

    style: 로고 추가

--- krampoline/src/assets/index.js
+++ krampoline/src/assets/index.js
@@ -4,6 +4,7 @@ import IcCorrectBlue from './icon/ic_correct_blue.svg?react';
 import IcCorrectGray from './icon/ic_correct_gray.svg?react';
 import IcFailBlue from './icon/ic_fail_blue.svg?react';
 import IcFailGray from './icon/ic_fail_gray.svg?react';
+import IcLogo from './icon/ic_logo.svg?react';
 import IcTest from './icon/icon_test.svg?react';
 
 // svg 더 추가하게 되면 ,(콤마)로 이어서 쓰기
@@ -14,5 +15,6 @@ export {
   IcCorrectGray,
   IcFailBlue,
   IcFailGray,
+  IcLogo,
   IcTest,
 };
