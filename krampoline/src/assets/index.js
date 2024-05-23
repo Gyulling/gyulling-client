a208e8228a187b51cbed8628098a9aa47d463982
Seung taek Oh <elqkddl@naver.com>
2024-05-24 05:55:32 +0900

    Chore: SVG index.js 설정

--- krampoline/src/assets/index.js
+++ krampoline/src/assets/index.js
@@ -4,21 +4,21 @@ import IcArrowBack from './icon/ic_arrow_back.svg?react';
 import IcCloseGray from './icon/ic_close_gray.svg?react';
 import IcCorrectBlue from './icon/ic_correct_blue.svg?react';
 import IcCorrectGray from './icon/ic_correct_gray.svg?react';
+import IcCup from './icon/ic_cup.svg?react';
 import IcFailBlue from './icon/ic_fail_blue.svg?react';
 import IcFailGray from './icon/ic_fail_gray.svg?react';
+import IcFish from './icon/ic_fish.svg?react';
+import IcHomeButton from './icon/ic_home_button.svg?react';
+import Ichome from './icon/ic_home_logo.svg?react';
 import IcLogo from './icon/ic_logo.svg?react';
+import IcMyInfo from './icon/ic_my_info.svg?react';
+import ICPlatinum from './icon/ic_platinum.svg?react';
+import IcQuizBack from './icon/ic_quiz_back.svg?react';
 import KaKaoAuth from './icon/kakao_auth.svg?react';
 import SuccessIcon from './icon/success.svg?react';
-
 // svg 더 추가하게 되면 ,(콤마)로 이어서 쓰기
 export {
-    Bomb,
-    IcArrowBack,
-    IcCloseGray,
-    IcCorrectBlue,
-    IcCorrectGray,
-    IcFailBlue,
-    IcFailGray,
-    IcLogo, KaKaoAuth, SuccessIcon
+    Bomb, ICPlatinum, IcArrowBack, IcCloseGray, IcCorrectBlue,
+    IcCorrectGray, IcCup, IcFailBlue, IcFailGray, IcFish, IcHomeButton, IcLogo, IcMyInfo, IcQuizBack, Ichome, KaKaoAuth, SuccessIcon
 };
 
