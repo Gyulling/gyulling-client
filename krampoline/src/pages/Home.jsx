1a25b6a9f046e949822947bb6dd50a92e2db2bd2
y2hjjh <y2hjjh@naver.com>
2024-05-24 03:40:40 +0000

    error fix

--- krampoline/src/pages/Home.jsx
+++ krampoline/src/pages/Home.jsx
@@ -6,7 +6,7 @@ import HomeHeader from '../component/Home/HomeHeader';
 import { api } from '../libs/api';
 
 const Home = () => {
-  const userId = sessionStorage.getItem("userId");
+  const userId = !sessionStorage.getItem("userId") ? 1 : sessionStorage.getItem("userId");
   const data = api.get(`/api/v1/user/${userId}/point`);
   const point = data.data.point;
   const navigate = useNavigate('/');
