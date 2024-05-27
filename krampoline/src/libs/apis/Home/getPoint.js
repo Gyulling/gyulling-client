329cb33d90d6ce9ad7b215c1532aa6c9c4a4018d
seoAreum <1971236@hansung.ac.kr>
2024-05-27 10:07:39 +0900

    feat: 포인트를 받아오는 api

---
+++ krampoline/src/libs/apis/Home/getPoint.js
@@ -0,0 +1,14 @@
+import { api } from '../../api';
+
+const getPoint = async () => {
+  const userId = !sessionStorage.getItem('userId')
+    ? 1
+    : sessionStorage.getItem('userId');
+
+  const { data } = await api.get(`/api/v1/user/${userId}/point`);
+  const { point } = data.data;
+
+  return { point };
+};
+
+export default getPoint;
