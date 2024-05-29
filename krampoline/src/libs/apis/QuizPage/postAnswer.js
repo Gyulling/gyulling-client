f48526316050195c98b65a6f7d0003e044aec947
seoAreum <1971236@hansung.ac.kr>
2024-05-29 14:03:13 +0900

    fix: 로그인 안하고 퀴즈 풀었을 경우 에러나는 문제 해결

--- krampoline/src/libs/apis/QuizPage/postAnswer.js
+++ krampoline/src/libs/apis/QuizPage/postAnswer.js
@@ -2,7 +2,9 @@ import { api } from '../../api';
 
 const postAnswer = async (correctIc) => {
   const quizId = sessionStorage.getItem('quizId');
-  const userId = sessionStorage.getItem('userId');
+  const userId = !sessionStorage.getItem('userId')
+    ? 1
+    : sessionStorage.getItem('userId');
 
   const { data } = await api.post(`/api/v1/quiz/${quizId}/solve/${userId}`, {
     answer: correctIc ? 1 : 0,
