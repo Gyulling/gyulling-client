cd4752bd8c1c06d1465fc9f7c84f80666eb73641
seoAreum <1971236@hansung.ac.kr>
2024-06-01 17:43:47 +0900

    chore: 타입 정의

---
+++ krampoline/src/types/CommonType/buttonType.ts
@@ -0,0 +1,7 @@
+// OnButton.tsx
+interface OnButtonProps {
+  children: React.ReactNode;
+  isQuiz: boolean;
+  disabled: boolean;
+  handleFn: () => void;
+}
---
+++ krampoline/src/types/PageType/pageType.ts
@@ -0,0 +1,3 @@
+export interface WaitingPageProps {
+  isLoadingPage: boolean;
+}
