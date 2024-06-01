3676c3d79a768defb373166513ebc1d9243db52f
seoAreum <1971236@hansung.ac.kr>
2024-06-01 18:12:07 +0900

    chore: 타입 정의

--- krampoline/src/types/ApiType/apiType.ts
+++ krampoline/src/types/ApiType/apiType.ts
@@ -1,3 +1,11 @@
-export interface postAnswerProps {
+export interface PostAnswerProps {
   correctIc: boolean;
+}
+
+export interface GetQuizContentProps {
+  quizId: number;
+}
+
+export interface PostKeywordsProps {
+  selectedCategory: Array<string>;
 }
--- krampoline/src/types/ComponentType/componentType.ts
+++ krampoline/src/types/ComponentType/componentType.ts
@@ -1,6 +1,18 @@
+import React from 'react';
+
 export interface SelectAnswerProps {
   correctIc: boolean;
   failIc: boolean;
   handleClickCorrectBtn: () => void;
   handleClickFailBtn: () => void;
+}
+
+// Article.tsx, RetryButton.tsx
+export interface ChildrenProps {
+  children: React.ReactNode;
+}
+
+export interface SelectTagProps {
+  selectedCategory: Array<string>;
+  handleClickCategory: (desc: string) => void;
 }
