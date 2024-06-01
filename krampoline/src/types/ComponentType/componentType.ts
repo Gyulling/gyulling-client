b521457b045349bf0c2d74dc2ec2eef5cd3e96ee
seoAreum <1971236@hansung.ac.kr>
2024-06-01 18:28:33 +0900

    chore: 타입 정의 위치 변경

--- krampoline/src/types/ComponentType/componentType.ts
+++ krampoline/src/types/ComponentType/componentType.ts
@@ -1,15 +1,8 @@
-import React from 'react';
-
 export interface SelectAnswerProps {
   correctIc: boolean;
   failIc: boolean;
   handleClickCorrectBtn: () => void;
   handleClickFailBtn: () => void;
-}
-
-// Article.tsx, RetryButton.tsx
-export interface ChildrenProps {
-  children: React.ReactNode;
 }
 
 export interface SelectTagProps {
