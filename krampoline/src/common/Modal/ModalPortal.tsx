9fb7d099d717f62ce21277b41ff315d18cee3964
seoAreum <1971236@hansung.ac.kr>
2024-06-05 22:20:02 +0900

    chore: 주석 추가

--- krampoline/src/common/Modal/ModalPortal.tsx
+++ krampoline/src/common/Modal/ModalPortal.tsx
@@ -3,6 +3,7 @@ import { ChildrenProps } from '../../types/CommonType/commonType';
 
 const ModalPortal = ({ children }: ChildrenProps) => {
   const el: HTMLElement | null = document.getElementById('modal');
+  // DOM 트리에 해당되지 않는 가상 메모리에 존재하는 DOM node 객체
   return ReactDOM.createPortal(children, el as Element | DocumentFragment);
 };
 
